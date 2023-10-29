1fea140d443cf6738a83d0915785ca1506910cb7
Evgenii Ermak <djermak3000@mail.ru>
2023-10-29 15:30:20 +0300

    update work - 2.0

--- HH_&_SJ/vacancy.py
+++ HH_&_SJ/vacancy.py
@@ -5,23 +5,12 @@ import os
 from abc import ABC, abstractmethod
 
 class APIManager(ABC):
-
     @abstractmethod
     def get_vacancies(self):
-        """
-        Получает вакансии по API и возвращает список вакансий согласно запросу пользователя.
-        """
         pass
 
-class Vakancy:
+class Vacancy:
     def __init__(self, name, page, top_n):
-        """
-        Инициализация объекта Vacancy.
-
-        :param name: Название вакансии.
-        :param page: Номер страницы.
-        :param top_n: Количество вакансий на странице.
-        """
         self.name = name
         self.page = page
         self.top_n = top_n
@@ -29,52 +18,43 @@ class Vakancy:
     def __repr__(self):
         return f'{self.name}'
 
-class HH(Vakancy, APIManager):
+class HH(Vacancy, APIManager):
     def __init__(self, name, page, top_n):
         super().__init__(name, page, top_n)
         self.url = 'https://api.hh.ru'
+        self.platform = 'HH'
 
     def get_vacancies(self):
-        """
-        Получает вакансии с помощью API и возвращает их в виде списка.
-
-        :return: Список вакансий.
-        """
         data = requests.get(f'{self.url}/vacancies', params={'text': self.name, 'page': self.page, 'per_page': self.top_n}).json()
         return data
 
     def load_vacancy(self):
-        """
-        Загружает вакансии с помощью API и возвращает список вакансий.
-
-        :return: Список вакансий.
-        """
         data = self.get_vacancies()
         vacancies = []
         for vacancy in data.get('items', []):
             published_at = datetime.strptime(vacancy['published_at'], "%Y-%m-%dT%H:%M:%S%z")
             vacancy_info = {
+                'platform': self.platform,
                 'id': vacancy['id'],
                 'name': vacancy['name'],
-                'solary_ot': vacancy['salary']['from'] if vacancy.get('salary') else None,
-                'solary_do': vacancy['salary']['to'] if vacancy.get('salary') else None,
+                'salary_from': vacancy['salary']['from'] if vacancy.get('salary') else None,
+                'salary_to': vacancy['salary']['to'] if vacancy.get('salary') else None,
                 'responsibility': vacancy['snippet']['responsibility'],
-                'data': published_at.strftime("%d.%m.%Y")
+                'date': published_at.strftime("%d.%m.%Y"),
+                'city': vacancy['area']['name'] if vacancy.get('area') else 'N/A',
+                'work_schedule': vacancy['schedule']['name'] if vacancy.get('schedule') else 'N/A'
+                # Добавьте другие детали, если они предоставлены API
             }
             vacancies.append(vacancy_info)
         return vacancies
 
-class Super_job(Vakancy, APIManager):
+class SuperJob(Vacancy, APIManager):
     def __init__(self, name, page, top_n):
         super().__init__(name, page, top_n)
         self.url = 'https://api.superjob.ru/2.0/vacancies/'
+        self.platform = 'SuperJob'
 
     def get_vacancies(self):
-        """
-        Получает вакансии с помощью API SuperJob и возвращает их в виде списка.
-
-        :return: Список вакансий.
-        """
         headers = {
             'X-Api-App-Id': os.getenv('API_KEY_SJ'),
         }
@@ -82,106 +62,56 @@ class Super_job(Vakancy, APIManager):
         return data
 
     def load_vacancy(self):
-        """
-        Загружает вакансии с помощью API SuperJob и возвращает список вакансий.
-
-        :return: Список вакансий.
-        """
         data = self.get_vacancies()
-        vacancy_list_SJ = []
-        for i in data['objects']:
-            published_at = datetime.fromtimestamp(i.get('date_published', ''))
+        vacancies = []
+        for vacancy in data['objects']:
+            published_at = datetime.fromtimestamp(vacancy.get('date_published', ''))
             super_job = {
-                'id': i['id'],
-                'name': i.get('profession', ''),
-                'solary_ot': i.get('payment_from', '') if i.get('payment_from') else None,
-                'solary_do': i.get('payment_to') if i.get('payment_to') else None,
-                'responsibility': i.get('candidat').replace('\n', '').replace('•', '') if i.get('candidat') else None,
-                'data': published_at.strftime("%d.%m.%Y"),
+                'platform': self.platform,
+                'id': vacancy['id'],
+                'name': vacancy.get('profession', ''),
+                'salary_from': vacancy.get('payment_from', '') if vacancy.get('payment_from') else None,
+                'salary_to': vacancy.get('payment_to') if vacancy.get('payment_to') else None,
+                'responsibility': vacancy.get('candidat', '').replace('\n', '').replace('•', '') if vacancy.get('candidat') else None,
+                'date': published_at.strftime("%d.%m.%Y"),
+                'city': vacancy.get('town', {}).get('title', 'N/A'),
+                'work_schedule': vacancy.get('schedule', {}).get('title', 'N/A')
+                # Добавьте другие детали, если они предоставлены API
             }
-            vacancy_list_SJ.append(super_job)
-        return vacancy_list_SJ
+            vacancies.append(super_job)
+        return vacancies
 
 def job_vacancy():
-    """
-    Основной код проекта, после ввода данных пользователем, данные сортируются согласно запросу пользователя и сохраняются в JSON файл.
-    Пользователь может выбрать платформу для поиска вакансий (HH, SJ, обе) и просматривать результаты поиска.
-
-    :return: None
-    """
     name = input('Введите вакансию: ')
     top_n = input('Введите кол-во вакансий: ')
     page = int(input('Введите номер страницы: '))
     hh_instance = HH(name, page, top_n)
-    sj_instance = Super_job(name, page, top_n)
-    combined_dict = {'HH': hh_instance.load_vacancy(), 'SJ': sj_instance.load_vacancy()}
+    sj_instance = SuperJob(name, page, top_n)
+    combined_list = hh_instance.load_vacancy() + sj_instance.load_vacancy()
 
-    with open('Super_job.json', 'w', encoding='utf-8') as file:
-        json.dump(combined_dict, file, ensure_ascii=False, indent=2)
+    with open('SuperJob.json', 'w', encoding='utf-8') as file:
+        json.dump(combined_list, file, ensure_ascii=False, indent=2)
 
-    platforma = input('Введите платформу для поиска: (1 - HH, 2 - SJ, 3 - обе платформы)  ')
+    platform_choice = input('Выберите платформу для поиска: (1 - HH, 2 - SuperJob, 3 - Обе) ')
 
-    if platforma == '1':
-        while True:
-            hh_instance.page = page
-            sj_instance.page = page
-            hh_data = hh_instance.load_vacancy()
+    while True:
+        if platform_choice == '1':
+            hh_vacancies = [vacancy for vacancy in combined_list if vacancy['platform'] == 'HH']
+            for platform in hh_vacancies:
+                print(f"\nПлатформа: {platform['platform']}\nID вакансии: {platform['id']}\nДата публикации: {platform['date']}\nДолжность: {platform['name']}\nЗарплата от: {platform['salary_from']}\nЗарплата до: {platform['salary_to']}\nОписание: {platform['responsibility']}\nГород: {platform['city']}\nГрафик работы: {platform['work_schedule']}\n")
 
-            combined_dict['HH'] = hh_data
+        elif platform_choice == '2':
+            sj_vacancies = [vacancy for vacancy in combined_list if vacancy['platform'] == 'SuperJob']
+            for platform in sj_vacancies:
+                print(f"\nПлатформа: {platform['platform']}\nID вакансии: {platform['id']}\nДата публикации: {platform['date']}\nДолжность: {platform['name']}\nЗарплата от: {platform['salary_from']}\nЗарплата до: {platform['salary_to']}\nОписание: {platform['responsibility']}\nГород: {platform['city']}\nГрафик работы: {platform['work_schedule']}\n")
 
-            with open('Super_job.json', 'w', encoding='utf-8') as file:
-                json.dump(combined_dict, file, ensure_ascii=False, indent=2)
+        elif platform_choice == '3':
+            for platform in combined_list:
+                print(f"\nПлатформа: {platform['platform']}\nID вакансии: {platform['id']}\nДата публикации: {platform['date']}\nДолжность: {platform['name']}\nЗарплата от: {platform['salary_from']}\nЗарплата до: {platform['salary_to']}\nОписание: {platform['responsibility']}\nГород: {platform['city']}\nГрафик работы: {platform['work_schedule']}\n")
 
-            for platform in combined_dict['HH']:
-                print(f"\nid - {platform['id']}\nДолжность - {platform['name']}\nЗ.п от - {platform['solary_ot']}\nЗ.п до - {platform['solary_ot']}\nОписание - {platform['responsibility']}\nДата - {platform['data']}\n")
-            a = input('перейти на следующую страницу? y/n ')
-            if a == 'y':
-                page += 1
-            else:
-                break
-    elif platforma == '2':
-        while True:
-            hh_instance.page = page
-            sj_instance.page = page
-            hh_data = hh_instance.load_vacancy()
-            sj_data = sj_instance.load_vacancy()
-
-            combined_dict['HH'] = hh_data
-            combined_dict['SJ'] = sj_data
-
-            with open('Super_job.json', 'w', encoding='utf-8') as file:
-                json.dump(combined_dict, file, ensure_ascii=False, indent=2)
-
-            for platform in combined_dict['SJ']:
-                print(f"\nid - {platform['id']}\nДолжность - {platform['name']}\nЗ.п от - {platform['solary_ot']}\nЗ.п до - {platform['solary_do']}\nОписание - {platform['responsibility']}\nДата - {platform['data']}\n")
-
-            a = input('перейти на следующую страницу? y/n ')
-            if a == 'y':
-                page += 1
-            else:
-                break
-    elif platforma == '3':
-        while True:
-            hh_instance.page = page
-            sj_instance.page = page
-            hh_data = hh_instance.load_vacancy()
-            sj_data = sj_instance.load_vacancy()
-
-            combined_dict['HH'] = hh_data
-            combined_dict['SJ'] = sj_data
-
-            with open('Super_job.json', 'w', encoding='utf-8') as file:
-                json.dump(combined_dict, file, ensure_ascii=False, indent=2)
-
-            for platform, data in combined_dict.items():
-                print(f"\n \033Платформа: {platform}")
-                for item in data:
-                    print(f"id - {item['id']}\nДолжность - {item['name']}\nЗ.п от - {item['solary_ot']}\nЗ.п до - {item['solary_do']}\nОписание - {item['responsibility']}\nДата - {item['data']}\n")
-
-            a = input('перейти на следующую страницу? y/n ')
-            if a == 'y':
-                page += 1
-            else:
-                break
+        next_page = input('Перейти на следующую страницу? (y/n) ')
+        if next_page.lower() != 'y':
+            break
+        page += 1
 
 job_vacancy()
